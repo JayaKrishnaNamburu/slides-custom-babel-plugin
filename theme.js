e8ed7ce610e572fa391978ee80c12145bc065a3d
JayaKrishna <namburu1995@gmail.com>
2019-01-02 21:46:05 +0530

    Slides

---
+++ theme.js
@@ -0,0 +1,24 @@
+import { yellow as theme } from 'mdx-deck/themes'
+
+console.log(theme);
+
+export default {
+  ...theme,
+  colors: {
+    background: '#3b3c38',
+    text: '#f5da55',
+    code: '#f5da55',
+    link: '#f5da55',
+    preBackground: '#f5da55',
+    pre: '#000'
+  },
+  blockquote: {
+    fontWeight: 400
+  },
+  pre: {
+    fontSize: '1.2rem'
+  },
+  heading: {
+    textTransform: 'capitalize'
+  }
+}
