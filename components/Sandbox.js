7706e0bde19400b94cc3b9eae709133bf53b7a9b
JayaKrishna <namburu1995@gmail.com>
2019-02-27 21:30:02 +0530

    Added code sandbox example

---
+++ components/Sandbox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Sandbox() {
+    return(
+        <iframe
+            src="https://codesandbox.io/embed/001zyj5xpn?autoresize=1&fontsize=14&hidenavigation=1"
+            style={{ width:"100%", height:"500px", border:0, borderRadius: "4px", overflow:"hidden" }}
+            sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"></iframe>    
+    );
+}
+
+export default Sandbox;
