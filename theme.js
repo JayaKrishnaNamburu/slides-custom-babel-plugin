b7b103ab526139263689fa71263926bc9cd95e7b
JayaKrishna <namburu1995@gmail.com>
2019-02-27 14:50:28 +0530

    Added Tweet from Addy

--- theme.js
+++ theme.js
@@ -3,10 +3,13 @@ import { yellow as theme } from 'mdx-deck/themes'
 export default {
   ...theme,
   colors: {
-    background: '#3b3c38',
-    text: '#f5da55',
+    // background: '#3b3c38',
+    background: '#fff',
+    // text: '#f5da55',
+    text: '#000',
     code: '#f5da55',
-    link: '#f5da55',
+    // link: '#f5da55',
+    link: '#000',
     preBackground: '#f5da55',
     pre: '#000'
   },
